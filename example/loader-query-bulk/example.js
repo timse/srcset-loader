391b7d3f20be195ead846637f02b9cf385eb0f0f
Tim Sebastian <tim.sebastian@gmail.com>
2016-10-07 12:38:06 +0200

    add examples how to use loader config sizes for bulk requires etc.

---
+++ example/loader-query-bulk/example.js
@@ -0,0 +1,24 @@
+function getImage(image) {
+    return require('./images/' + image);
+};
+
+// loaded via srcset loader as specified in the webpack config
+['whale.jpeg', 'paris.jpeg'].map(img => getImage(img)).forEach((src) => {
+    const image = new Image();
+    image.srcset = src.srcSet;
+    image.src = src.sources['800w'];
+    image.sizes = '(min-width: 1000px) 500px, 200px';
+    image.style = 'width: auto';
+    document.body.appendChild(image);
+});
+
+const whale = require('../whale.jpeg');
+const paris = require('../paris.jpeg');
+
+// just normal images as they are not in `/images/`
+[whale, paris].forEach((src) => {
+  const image = new Image();
+  image.src = src
+  image.style = 'width: 100%';
+  document.body.appendChild(image);
+});
