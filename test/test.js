a0d8c79aaa51d35bcc96adefd6ea4272d74d6269
Paul Kiddle <paul.kiddle@nice.agency>
2017-05-31 15:23:08 +0100

    Revert test changes

--- test/test.js
+++ test/test.js
@@ -178,17 +178,17 @@ describe('Resource Query', () => {
 
       return new Promise((resolve, reject) => {
         const imgTag = window.Image();
-        imgTag.addEventListener('load', function onLoad() {
+        imgTag.onload = function onLoad() {
           console.log(imgTag.naturalWidth);
           console.log(imgTag.naturalHeight);
 
           console.log(imgTag);
           resolve();
-        });
+        };
 
-        imgTag.addEventListener('error', function onError(e) {
+        imgTag.onerror = function onError(e) {
           reject(e);
-        });
+        };
 
         imgTag.src = img.placeholder.url;
       });
